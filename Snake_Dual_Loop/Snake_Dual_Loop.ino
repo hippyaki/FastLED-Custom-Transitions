abbc36c5ba175fcc07cbfec11b7db8a4e6c9cd47
Akshayan Sinha <akshayan.sinha@gmail.com>
2021-11-03 23:21:12 +0530

    added Snake Transitions

---
+++ Snake_Dual_Loop/Snake_Dual_Loop.ino
@@ -0,0 +1,49 @@
+#include <FastLED.h>
+#define LED_PIN     5
+#define NUM_LEDS    60
+
+CRGB leds[NUM_LEDS];
+void setup() {
+  Serial.begin(9600);
+  delay(3000);
+  FastLED.addLeds<WS2812B, LED_PIN, GRB>(leds, NUM_LEDS);
+  
+}
+void loop() 
+{ 
+  for (int x = 1;x<NUM_LEDS; x++){
+    
+    leds[x] = CRGB(255, 0, 0);
+    leds[x-1] = CRGB(0, 255, 0);
+    FastLED.show();
+    
+    Serial.print("F -> ");
+    Serial.println(x); 
+    
+    delay(20); 
+    
+    leds[x] = CRGB(0, 0, 0);
+    leds[x-1] = CRGB(0, 0, 0);
+    
+  }
+  
+  for (int x = NUM_LEDS-1;x>=1; x--){
+    
+    leds[x-1] = CRGB(255, 0, 0);
+    leds[x] = CRGB(0, 255, 0);
+
+    FastLED.show();
+    
+    Serial.print("B -> ");
+    Serial.println(x);
+
+    delay(20);
+    
+    leds[x] = CRGB(0, 0, 0);
+    leds[x-1] = CRGB(0, 0, 0);
+    
+     
+  }
+
+  
+}
---
+++ Snake_Food_Advanced/Snake_Food_Advanced.ino
@@ -0,0 +1,62 @@
+#include <FastLED.h>
+#define LED_PIN     5
+#define NUM_LEDS    60
+
+int z=1;
+CRGB leds[NUM_LEDS];
+void setup() {
+  Serial.begin(9600);
+  delay(3000);
+  FastLED.addLeds<WS2812B, LED_PIN, GRB>(leds, NUM_LEDS);
+  
+}
+void loop() 
+{ 
+  for (int x = z; x<NUM_LEDS; x++){
+    
+    leds[x] = CRGB(255, 0, 0);
+    
+    for (int y=1; y<z;y++){
+      leds[x-y] = CRGB(0, 255, 0);
+      Serial.print(" Loop -");
+      Serial.print(y);
+    }
+    
+    FastLED.show();
+    
+    Serial.print("- F -> ");
+    Serial.println(x); 
+    
+    delay(20); 
+    
+    leds[x] = CRGB(0, 0, 0);
+    
+    for (int y=1; y<z;y++){
+      leds[x-y] = CRGB(0, 0, 0);
+    }
+  }
+  z++;
+  //Serial.println(z);
+  
+  for (int x = NUM_LEDS-1;x>=z; x--){
+    
+    leds[x-z+1] = CRGB(255, 0, 0);
+    for (int y=1; y<z;y++){
+      leds[x-z+1+y] = CRGB(0, 255, 0);
+      Serial.print(" Loop -");
+      Serial.print(y);
+    }
+
+    FastLED.show();
+    Serial.print("B -> ");
+    Serial.println(x);
+
+    delay(20);
+    
+    leds[x-z+1] = CRGB(0, 0, 0);
+    for (int y=1; y<z;y++){
+      leds[x-z+1+y] = CRGB(0, 0, 0);
+    }  
+  } 
+  z++;
+}
